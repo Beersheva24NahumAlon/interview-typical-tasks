fc9c155ec996c6cb1b7dbe46ba4e155ab6ec9ebe
Nahum Alon <nochum.shepsl@gmail.com>
2024-09-28 17:50:10 +0300

    Fixed method hasSumTwo in class InterviewTasks

--- src/main/java/telran/interview/InterviewTasks.java
+++ src/main/java/telran/interview/InterviewTasks.java
@@ -5,16 +5,10 @@ import java.util.*;
 public class InterviewTasks {
     static public boolean hasSumTwo(int[] array, int sum) {
         HashSet<Integer> set = new HashSet<>();
-        Arrays.stream(array).forEach(i -> set.add(i));
-        Iterator<Integer> it = set.iterator();
-        Integer cur = it.next();
-        while (it.hasNext() && !isContainElementForSum(set, cur, sum)) {
-            cur = it.next();
+        int i = 0;
+        while (i < array.length && !set.contains(sum - array[i])) {
+            set.add(array[i++]);
         }
-        return it.hasNext();
-    }
-
-    private static boolean isContainElementForSum(HashSet<Integer> set, Integer cur, Integer sum) {
-        return set.contains(sum - cur) && cur != sum - cur;
-    }   
+        return i != array.length;
+    }  
 }
--- src/test/java/telran/interview/InterviewTasksTest.java
+++ src/test/java/telran/interview/InterviewTasksTest.java
@@ -11,8 +11,10 @@ public class InterviewTasksTest {
     void hasSumTwoTest() {
         int sum = 8;
         int[] arrToTrue = { 2, 1, 3, 4, 7 };
+        int[] arrToTrue2 = { 1, 2, 4, 4, 10 };
         int[] arrToFalse = { 1, 2, 3, 4, 10 };
         assertTrue(hasSumTwo(arrToTrue, sum));
+        assertTrue(hasSumTwo(arrToTrue2, sum));
         assertFalse(hasSumTwo(arrToFalse, sum));
     }
 }
