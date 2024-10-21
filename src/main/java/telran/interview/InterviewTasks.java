136a4a1999f28f55c26f413cc922585e411db6be
Nahum Alon <nochum.shepsl@gmail.com>
2024-10-21 23:25:44 +0300

    Added methods in class InterviewTasks and tests for them

--- src/main/java/telran/interview/InterviewTasks.java
+++ src/main/java/telran/interview/InterviewTasks.java
@@ -1,6 +1,12 @@
 package telran.interview;
 
 import java.util.*;
+import java.util.function.Function;
+import java.time.LocalDate;
+import java.util.stream.*;
+
+record DateRole(LocalDate date,String role) {
+}
 
 public class InterviewTasks {
     static public boolean hasSumTwo(int[] array, int sum) {
@@ -10,5 +16,41 @@ public class InterviewTasks {
             set.add(array[i++]);
         }
         return i != array.length;
-    }  
+    }
+
+    public static int getMaxWithNegativePresentation(int[] array) {
+        int res = -1;
+        int elementAbs = -1;
+        HashSet<Integer> set = new HashSet<>();
+        for (int element : array) {
+            elementAbs = Math.abs(element);
+            if (set.contains(-element)) {
+                res = elementAbs > res ? elementAbs : res;
+            } else {
+                set.add(element);
+            }
+        }
+        return res;
+    }
+
+    public static List<DateRole> assignRoleDates(List<DateRole> rolesHistory,
+            List<LocalDate> dates) {
+        TreeMap<LocalDate, String> map = new TreeMap<>();
+        List<DateRole> res = new ArrayList<DateRole>();
+        rolesHistory.stream().forEach(dr -> map.put(dr.date(), dr.role()));
+        dates.stream().forEach(d -> res.add(new DateRole(d, map.floorKey(d) != null ? map.get(map.floorKey(d)) : null)));
+        return res;
+    }
+
+    public static boolean isAnagram(String word, String anagram) {
+        boolean res = false;
+        if (word.length() == anagram.length() && word != anagram) {
+            res = Objects.equals(getMapFromString(word), getMapFromString(anagram));
+        } 
+        return res;
+    }
+
+    private static Map<Integer, Long> getMapFromString(String word) {
+        return word.chars().boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
 }
--- src/test/java/telran/interview/InterviewTasksTest.java
+++ src/test/java/telran/interview/InterviewTasksTest.java
@@ -4,8 +4,17 @@ import static telran.interview.InterviewTasks.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.util.List;
 
 public class InterviewTasksTest {
+    private static final LocalDate DATE1 = LocalDate.of(2019, 12, 31);
+	private static final LocalDate DATE2 = LocalDate.of(2021, 10, 30);
+	private static final LocalDate DATE3 = LocalDate.of(2021, 12, 31);
+	private static final LocalDate DATE4 = LocalDate.of(2024, 1, 1);
+	private static final String ROLE1 = "Developer";
+	private static final String ROLE2 = "Senior Developer";
+	private static final String ROLE3 = "Manager";
 
     @Test
     void hasSumTwoTest() {
@@ -16,5 +25,49 @@ public class InterviewTasksTest {
         assertTrue(hasSumTwo(arrToTrue, sum));
         assertTrue(hasSumTwo(arrToTrue2, sum));
         assertFalse(hasSumTwo(arrToFalse, sum));
+    }
+
+    @Test
+    void getMaxWithNegativePresentationTest() {
+        int[] array1 = { 200, 10, -10, 1, 2, 3, -3, -200 };
+        int[] array2 = { -200, -200, 100, 10, 10 };
+        int[] array3 = { -200, 10, -10, 1, 2, 3, -3, 200 };
+        assertEquals(200, getMaxWithNegativePresentation(array1));
+        assertEquals(-1, getMaxWithNegativePresentation(array2));
+        assertEquals(200, getMaxWithNegativePresentation(array3));
+    }
+
+    @Test
+    void assignRoleDatesTest() {
+        List<DateRole> history = List.of(
+                new DateRole(LocalDate.of(2020, 1, 1), ROLE1),
+                new DateRole(LocalDate.of(2021, 1, 1), ROLE2),
+                new DateRole(LocalDate.of(2022, 1, 1), ROLE3)
+        );
+        List<LocalDate> dates = List.of(
+                DATE1,
+                DATE2, 
+                DATE3,
+                DATE4
+        );
+        List<DateRole> expected = List.of(
+                new DateRole(DATE1, null),
+                new DateRole(DATE2, ROLE2),
+                new DateRole(DATE3, ROLE2),
+                new DateRole(DATE4, ROLE3)
+        );
+        assertIterableEquals(expected, assignRoleDates(history, dates));
+    }
+
+    @Test
+    void isAnagrmaTest() {
+        String word = "hello";
+        assertTrue(isAnagram(word, "olleh"));
+        assertTrue(isAnagram(word, "elloh"));
+        assertTrue(isAnagram(word, "hleol"));
+        assertFalse(isAnagram(word, word));
+        assertFalse(isAnagram(word, "olle"));
+        assertFalse(isAnagram(word, "ollhh"));
+        assertFalse(isAnagram(word, "olehd"));
     }
 }
