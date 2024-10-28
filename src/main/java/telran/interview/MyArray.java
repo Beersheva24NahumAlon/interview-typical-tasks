4b2b72ef3f2958488503384bbd950ebe6317ef1a
Nahum Alon <nochum.shepsl@gmail.com>
2024-10-28 21:15:55 +0200

    Added comment to classes MyArray and MyStackInt

--- src/main/java/telran/interview/MyArray.java
+++ src/main/java/telran/interview/MyArray.java
@@ -3,11 +3,16 @@ package telran.interview;
 import java.util.*;
 
 public class MyArray<T> {
+    // Value for filling array
     T valueForAll;
+    // Size of array 
     int size;
+    // Here I use data strucrure HashMap, because it has complexity O(1) for adding and getting data
+    // As a key we use index in array and object of data as a value
     HashMap<Integer, T> map;
 
     public void setAll(T value) {
+        // We create new map and define value for filling
         map = new HashMap<>();
         valueForAll = value;
     }
@@ -17,6 +22,7 @@ public class MyArray<T> {
         map.put(index, value);
     }
 
+    // In this method we check bounds of array and throw exception
     private void checkIndex(int index) {
         if (index > size - 1 || index < 0) {
             throw new ArrayIndexOutOfBoundsException();
@@ -25,6 +31,7 @@ public class MyArray<T> {
 
     public T get(int index) {
         checkIndex(index);
+        // We check if for some index we set some data, if it is not - return value for filling
         T res = map.get(index);
         return res != null ? res : valueForAll;
     }
--- src/main/java/telran/interview/MyStackInt.java
+++ src/main/java/telran/interview/MyStackInt.java
@@ -3,7 +3,14 @@ package telran.interview;
 import java.util.*;
 
 public class MyStackInt {
+    // I use data structures ArrayList (actualy, in our case it's not metter wich list we chouse,
+    // because operations adding last element and getting last element of ArrayList and LinkedList 
+    // have same complaxity O(1))
+
+    // Here we will store all elements of our stack 
     private ArrayList<Integer> array = new ArrayList<>();
+    // Here we will store all maximum elements of our stack in increasing order
+    // to return maximum element of stack with complaxity O(1) (return last element of this list)
     private ArrayList<Integer> arrayMax = new ArrayList<>();
 
     public void push(int num) {
@@ -14,10 +21,15 @@ public class MyStackInt {
         
     }
 
-    public int pop() {
+    // In this method we check if stack is empty and throw exception
+    private void checkEmpty() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
+    }
+
+    public int pop() {
+        checkEmpty();
         int res = array.getLast();
         array.removeLast();
         if (res == arrayMax.getLast()) {
@@ -27,9 +39,7 @@ public class MyStackInt {
     }
 
     public int peek() {
-        if (isEmpty()) {
-            throw new EmptyStackException();
-        }
+        checkEmpty();
         return array.getLast();
     }
 
@@ -38,9 +48,7 @@ public class MyStackInt {
     }
 
     public int getMaxElement() {
-        if (isEmpty()) {
-            throw new EmptyStackException();
-        }
+        checkEmpty();
         return arrayMax.getLast();
     }
 }
